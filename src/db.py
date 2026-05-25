c8f8c61de878826cb8e386e910c9ed5f8c8100f2
fsalahi <53266505+fsalahi@users.noreply.github.com>
2026-05-25 12:07:50 -0400

    separation of configuration and application

--- src/db.py
+++ src/db.py
@@ -1,12 +1,34 @@
 
-# We are creating db connection file
+# # Version 1: We are creating db connection file, tentatively here, 
+# # however this is not good practice
+# from sqlalchemy import create_engine
+
+# # DB_USER = "postgres"
+# # DB_PASSWORD = "112358"
+# # DB_HOST = "localhost"
+# # DB_PORT = "5432"
+# # DB_NAME = "crypto_pipeline"
+
+# DATABASE_URL = (
+#     f"postgresql://{DB_USER}:{DB_PASSWORD}"
+#     f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# )
+
+# engine = create_engine(DATABASE_URL)
+
+# version 2: separating code and config
+
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
-DB_USER = "postgres"
-DB_PASSWORD = "112358"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "crypto_pipeline"
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
 DATABASE_URL = (
     f"postgresql://{DB_USER}:{DB_PASSWORD}"
--- src/main.py
+++ src/main.py
@@ -1,11 +1,3 @@
-import logging
-from extract import extract_crypto_data
-from transform import transform_data
-from extract import extract_crypto_data
-from extract import extract_crypto_data
-from transform import transform_data
-from load import load_data
-
 # test connection:
 # from db import engine
 # print(engine)
@@ -21,17 +13,31 @@ from load import load_data
 
 # print(df.head())
 
+# # debug
+# import sys
+# # Prints the path to the active venv (or the global Python path if no venv)
+# print(sys.prefix)
+
+# # Returns True if you are inside a venv, False if not
+# is_venv = sys.prefix != sys.base_prefix
+# print(f"Is Venv Active: {is_venv}")
+
 # connecting the entire pipeline
+from extract import extract_crypto_data
 raw_data = extract_crypto_data()
 
+from transform import transform_data
 df = transform_data(raw_data)
 
+
+from load import load_data
 load_data(df)
 
 print("Pipeline completed")
 
 
 # Logging
+import logging
 logging.basicConfig(
     filename='logs/pipeline.log',
     level=logging.INFO
